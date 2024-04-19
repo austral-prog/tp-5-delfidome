8277705c0157963afdc2b5ccc9726cce20a03817
delfidome <162723242+delfidome@users.noreply.github.com>
2024-04-19 18:36:47 -0300

    Update number_to_month.py

--- number_to_month.py
+++ number_to_month.py
@@ -1,13 +1,26 @@
-def number_to_month(month):
-    if month <= 1 or month <= 12:
-        months = [
-            'January', 'February', 'March', 'April', 'May', 'June', 'July',
-            'August', 'September', 'October', 'November', 'December'
-            ]
-        return months [month - 1]
-    else:
-        return "error"
-
-    print(number_to_month(1))
-    print(number_to_month(2))
-    print(number_to_month(13))
+# Replace the "AANSWER HERE
+ def number_to_month(month):
+     if month == 1:
+        return "January"
+    elif month == 2:
+        return "February"
+    elif month == 3:
+        return "March"
+    elif month == 4:
+        return "April"
+    elif month == 5:
+        return "May"
+    elif month == 6:
+        return "June"
+    elif month == 7:
+        return "July"
+    elif month == 8:
+        return "August"
+    elif month == 9:
+        return "September"
+    elif month == 10:
+        return "October"
+    elif month == 11:
+        return "November"
+     else:
+        return "December"
