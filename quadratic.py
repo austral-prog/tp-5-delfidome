38e7c778cc1f16ac0f7e203c108c0bd51f174da3
delfidome <162723242+delfidome@users.noreply.github.com>
2024-04-20 15:37:56 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,6 +1,6 @@
 import math
 def roots(a, b, c):
-    var1 = (b**2 - 4*a*c)
+    var1 = b**2 - 4*a*c
     if var1 < 0:
         return "( )"
         
@@ -19,7 +19,7 @@ def value_y(a, b, c, x):
 def to_string(a, b, c):
     #a y b y c
     if a and b and c:
-        return f"f(x) = {a}* X^2 + {b}* X + {c}"
+        return f"f(x) = {a} * X^2 + {b} * X + {c}"
     #b y c
     elif b and c:
         return f"f(x) = {b} * X + {c}"
