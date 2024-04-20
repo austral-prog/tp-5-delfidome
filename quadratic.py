36d328156c7b74d2a43588ffe13f25ecf0feb9f4
delfidome <162723242+delfidome@users.noreply.github.com>
2024-04-20 15:31:22 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,11 +1,9 @@
-# Replace the "ANSWER HERE" for your answer
 import math
-
 def roots(a, b, c):
-    return "ANSWER HERE"
     var1 = (b**2 - 4*a*c)
     if var1 < 0:
         return "( )"
+        
     raiz = math.sqrt(var1)
     resultado1= (-b+raiz)/2*a
     resultado2= (-b-raiz)/2*a
@@ -14,22 +12,14 @@ def roots(a, b, c):
     else:
         return f"({resultado1}, {resultado2})"
 
-
-
-
-
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
     imagen= a*(x**2) + b*x + c
     return imagen
 
-
-
 def to_string(a, b, c):
-    return "ANSWER HERE"
     #a y b y c
     if a and b and c:
-        return f"f(x) = {a}* x^2 + {b}* x + {c}"
+        return f"f(x) = {a}* X^2 + {b}* X + {c}"
     #b y c
     elif b and c:
         return f"f(x) = {b} * X + {c}"
@@ -40,12 +30,10 @@ def to_string(a, b, c):
     elif not a and not b and c:
         return f"f(x) = {c}"
 
-
 def derivation(a, b, c):
-    return "ANSWER HERE"
     if a and b:
         return f"f'(x) = {2 * a} * X + {b}"
     elif not a:
-        return f"f'(x) = {c}"
+        return f"f'(x) = {b}"
     elif not b:
         return f"f'f(x) = {2 * a} * X"
