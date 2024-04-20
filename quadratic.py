b1ce49800372426b2f74d2b33850769680aa6315
delfidome <162723242+delfidome@users.noreply.github.com>
2024-04-20 12:54:22 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,6 +1,8 @@
 # Replace the "ANSWER HERE" for your answer
 import math
+
 def roots(a, b, c):
+    return "ANSWER HERE"
     var1 = (b**2 - 4*a*c)
     if var1 < 0:
         return "( )"
@@ -17,12 +19,14 @@ def roots(a, b, c):
 
 
 def value_y(a, b, c, x):
+    return "ANSWER HERE"
     imagen= a*(x**2) + b*x + c
     return imagen
 
 
 
 def to_string(a, b, c):
+    return "ANSWER HERE"
     #a y b y c
     if a and b and c:
         return f"f(x) = {a}* x^2 + {b}* x + {c}"
@@ -38,6 +42,7 @@ def to_string(a, b, c):
 
 
 def derivation(a, b, c):
+    return "ANSWER HERE"
     if a and b:
         return f"f'(x) = {2 * a} * X + {b}"
     elif not a:
